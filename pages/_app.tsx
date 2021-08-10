f7d67094a0da3fdb69d96ac659a3126a16dfdcbd
Creo-KR <gjwlsdn0877@naver.com>
2021-08-10 17:11:00 +0900

    redux

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,16 @@
-import type { AppProps /*, AppContext */ } from "next/app";
-
+import { Provider } from "react-redux";
+import type { AppProps } from "next/app";
+import { store } from "../redux/store";
 import Layout from "../components/layout";
 
-const MyApp = ({ Component, pageProps }: AppProps) => (
-  <Layout>
-    <Component {...pageProps} />
-  </Layout>
-);
+function App({ Component, pageProps }: AppProps) {
+  return (
+    <Provider store={store}>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </Provider>
+  );
+}
 
-export default MyApp;
+export default App;
